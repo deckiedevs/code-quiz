9187a9461788294cdbb8d0f4cd2374ad37be11e9
Mila Decker <milacdecker@gmail.com>
2020-11-06 11:20:21 -0500

    added question iteration and answer checks

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ var codeQuiz = [
         b: 'Answer 1B',
         c: 'Answer 1C',
         d: 'Answer 1D',
-        correctAns: 'Answer 1B'
+        answer: 'b'
     }, 
     {
         question: 'Test question 2',
@@ -14,7 +14,7 @@ var codeQuiz = [
         b: 'Answer 2B',
         c: 'Answer 2C',
         d: 'Answer 2D',
-        correctAns: 'Answer 2C'
+        answer: 'c'
     },
     {
         question: 'Test question 3',
@@ -22,7 +22,7 @@ var codeQuiz = [
         b: 'Answer 3B',
         c: 'Answer 3C',
         d: 'Answer 3D',
-        correctAns: 'Answer 3C'
+        answer: 'c'
     },
     {
         question: 'Test question 4',
@@ -30,7 +30,7 @@ var codeQuiz = [
         b: 'Answer 4B',
         c: 'Answer 4C',
         d: 'Answer 4D',
-        correctAns: 'Answer 4D'
+        answer: 'd'
     },
     {
         question: 'Test question 5',
@@ -38,70 +38,125 @@ var codeQuiz = [
         b: 'Answer 5B',
         c: 'Answer 5C',
         d: 'Answer 5D',
-        correctAns: 'Answer 5B'
+        answer: 'b'
     }
 ]
 
 var cardEl = document.querySelector("#card");
 var startBtn = document.querySelector("#start-btn");
 var headerEl = document.querySelector("header");
+var questionCounter = 0;
+var currentScore = 100;
 // var scoreSpan = document.querySelector("#score");
 
 // start quiz function
 var createQuiz = function() {
     document.querySelector("#instructions").remove();
 
-    // creates container div for questions
-    var questionEl = document.createElement("div");
-    questionEl.className = "question";
-    cardEl.appendChild(questionEl);
+     // creates container div for questions
+     var questionEl = document.createElement("div");
+     questionEl.className = "question";
+     cardEl.appendChild(questionEl);
+ 
+     // creates container div for answers
+     var answersEl = document.createElement("div");
+     answersEl.className = "answer-grid";
+     cardEl.appendChild(answersEl);
+ 
+     // button A
+     var choiceA = document.createElement("div");
+     choiceA.className = "answer-grid-item";
+     answersEl.appendChild(choiceA);
+ 
+     var btnA = document.createElement("button");
+     btnA.className = "btn";
+     btnA.id = "btn-a";
+     btnA.setAttribute("value", "a");
+     choiceA.appendChild(btnA);
+ 
+     // button B
+     var choiceB = document.createElement("div");
+     choiceB.className = "answer-grid-item";
+     answersEl.appendChild(choiceB);
+ 
+     var btnB = document.createElement("button");
+     btnB.className = "btn";
+     btnB.id = "btn-b";
+     btnB.setAttribute("value", "b");
+     choiceB.appendChild(btnB);
+ 
+     // button C
+     var choiceC = document.createElement("div");
+     choiceC.className = "answer-grid-item";
+     answersEl.appendChild(choiceC);
+ 
+     var btnC = document.createElement("button");
+     btnC.className = "btn";
+     btnC.id = "btn-c";
+     btnC.setAttribute("value", "c");
+     choiceC.appendChild(btnC);
+ 
+     // button D
+     var choiceD = document.createElement("div");
+     choiceD.className = "answer-grid-item";
+     answersEl.appendChild(choiceD);
+ 
+     var btnD = document.createElement("button");
+     btnD.className = "btn";
+     btnD.id = "btn-d";
+     btnD.setAttribute("value", "d");
+     choiceD.appendChild(btnD);
 
-    // creates container div for answers
-    var answersEl = document.createElement("div");
-    answersEl.className = "answer-grid";
-    cardEl.appendChild(answersEl);
+    //  feedback div
+    var feedbackEl = document.createElement("div");
+    feedbackEl.className = "feedback";
+    cardEl.appendChild(feedbackEl);
 
-    // button A
-    var choiceA = document.createElement("div");
-    choiceA.className = "answer-grid-item";
-    answersEl.appendChild(choiceA);
-
-    var btnA = document.createElement("button");
-    btnA.className = "btn";
-    btnA.id = "btn-a";
-    choiceA.appendChild(btnA);
-
-    // button B
-    var choiceB = document.createElement("div");
-    choiceB.className = "answer-grid-item";
-    answersEl.appendChild(choiceB);
-
-    var btnB = document.createElement("button");
-    btnB.className = "btn";
-    btnB.id = "btn-b";
-    choiceB.appendChild(btnB);
-
-    // button C
-    var choiceC = document.createElement("div");
-    choiceC.className = "answer-grid-item";
-    answersEl.appendChild(choiceC);
-
-    var btnC = document.createElement("button");
-    btnC.className = "btn";
-    btnC.id = "btn-c";
-    choiceC.appendChild(btnC);
-
-    // button D
-    var choiceD = document.createElement("div");
-    choiceD.className = "answer-grid-item";
-    answersEl.appendChild(choiceD);
-
-    var btnD = document.createElement("button");
-    btnD.className = "btn";
-    btnD.id = "btn-d";
-    choiceD.appendChild(btnD);
-    
+    nextQues(questionCounter);
     scoreCounter();
+}
+
+var nextQues = function(index) {
+    var questionEl = document.querySelector(".question");
+    var btnA = document.getElementById("btn-a");
+    var btnB = document.getElementById("btn-b");
+    var btnC = document.getElementById("btn-c");
+    var btnD = document.getElementById("btn-d");
+    var allBtns = document.querySelector(".answer-grid"); 
+
+
+    questionEl.textContent = codeQuiz[index].question;
+    btnA.textContent = codeQuiz[index].a;
+    btnB.textContent = codeQuiz[index].b;
+    btnC.textContent = codeQuiz[index].c;
+    btnD.textContent = codeQuiz[index].d;
+
+    allBtns.addEventListener("click", checkAnswer);
+}
+
+var checkAnswer = function(event) {
+    var clickedBtn = event.target.getAttribute("value");
+    var backgroundColor = document.querySelector("body");
+    var feedbackEl = document.querySelector(".feedback");
+    
+    if (clickedBtn === codeQuiz[questionCounter].answer) {
+        backgroundColor.className = "correct";
+        feedbackEl.textContent = "CORRECT!"
+    }
+    else {
+        currentScore -= 5;
+        backgroundColor.className = "incorrect";
+        feedbackEl.textContent = "INCORRECT!"
+    }
+
+    questionCounter++
+
+    if (questionCounter < codeQuiz.length) {        
+        nextQues(questionCounter);
+    }
+    else {
+        console.log("Stop quiz here")
+    }
 }
 
 var scoreCounter = function () {
@@ -109,7 +164,6 @@ var scoreCounter = function () {
     scoreDiv.className = "score";
     headerEl.appendChild(scoreDiv);
 
-    var currentScore = 100;
     var scoreInterval = setInterval(function() {
         if (currentScore >= 0) {
             scoreDiv.textContent = "Current score: " + currentScore;
